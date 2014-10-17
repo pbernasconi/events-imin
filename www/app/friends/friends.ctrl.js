5a888236de6254333569798daca9aef87f767550
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-10-17 01:07:56 -0400

    Footbal friends

--- www/app/friends/friends.ctrl.js
+++ www/app/friends/friends.ctrl.js
@@ -2,7 +2,9 @@ angular.module('friends.ctrl', [])
 
     .controller('FriendsCtrl', function ($scope, $state, AuthService) {
 
-        AuthService.api("me/friends", ["user_friends"]).then(function (result) {
+        $scope.filterFriends = '';
+
+        AuthService.api("me/taggable_friends", ["user_friends"]).then(function (result) {
             $scope.friends = result;
             console.table(result);
         }, function (error) {
